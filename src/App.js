01fa4e320ec6ebe178c810935ae6d9506b97a45f
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-09-21 20:46:26 -0300

    Converts code to JSX

--- src/App.js
+++ src/App.js
@@ -4,24 +4,13 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Lulu",
-      animal: "Cat",
-      breed: "From the street",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Lulu" animal="Cat" breed="From the street" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
 };
-
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
