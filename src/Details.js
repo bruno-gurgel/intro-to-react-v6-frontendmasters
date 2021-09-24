391a3b6d4e4dc9172a618a2be380cad84c09548e
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-09-24 09:05:20 -0300

    Adjusts babel and includes state in class components

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
-import { withRouter } from "react-router";
+import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-  constructor() {
-    super();
+  state = { loading: true };
+  //   constructor() {
+  //     super();
 
-    this.state = { loading: true };
-  }
+  //     this.state = { loading: true };
+  //   }
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
